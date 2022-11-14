012e6cebf23abb678f49a83a05a15a44172e3728
Nadya-a <nad.shewczowa@ya.ru>
2022-11-14 21:17:07 +0300

    Добавьте файлы проекта.

---
+++ Самостоялка1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Самостоялка1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] Mass = File.ReadAllLines(@"C:\Users\Nadya\Desktop\тест.txt", System.Text.Encoding.Default);
+            task1.Text = Mass[0];
+            task2.Text = Mass[2];
+        }
+
+        private void Button_Check(object sender, RoutedEventArgs e)
+        {
+            string[] Mass = File.ReadAllLines(@"C:\Users\Nadya\Desktop\тест.txt", System.Text.Encoding.Default);
+
+
+            if (num1.Text == Mass[1])
+                num1.Background = Brushes.LightGreen;  
+            else
+                num1.Background = Brushes.LightBlue;
+
+            if (num2.Text == Mass[3])
+                num2.Background = Brushes.LightGreen;
+            else
+                num2.Background = Brushes.LightBlue;
+
+            if (num1.Text == Mass[1] && num2.Text == Mass[3])
+            {
+                Check.Content = "Bcë правильно";
+                Check.Background = Brushes.Green;
+            }
+            else if (num1.Text == Mass[1] || num2.Text == Mass[3])
+            {
+                Check.Content = "Где-то ошибка";
+                Check.Background = Brushes.Yellow;
+            }
+            else
+            {
+                Check.Content = "Неправильно";
+                Check.Background = Brushes.Red;
+            }
+        }
+
+        private void Button_Next(object sender, RoutedEventArgs e)
+        {
+            Window1 Window1 = new Window1();
+            this.Close();
+            Window1.ShowDialog();
+        }
+    }
+}
---
+++ Самостоялка1/Window1.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace Самостоялка1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            string[] Mass = File.ReadAllLines(@"C:\Users\Nadya\Desktop\тест.txt", Encoding.Default);
+            task1.Text = Mass[4];
+            task2.Text = Mass[6];
+        }
+
+        private void Button_Check(object sender, RoutedEventArgs e)
+        {
+            string[] Mass = File.ReadAllLines(@"C:\Users\Nadya\Desktop\тест.txt", System.Text.Encoding.Default);
+
+            if (num1.Text == Mass[5])
+                num1.Background = Brushes.LightGreen;
+            else
+                num1.Background = Brushes.LightBlue;
+
+            if (num2.Text == Mass[7])
+                num2.Background = Brushes.LightGreen;
+            else
+                num2.Background = Brushes.LightBlue;
+
+            if (num1.Text == Mass[5] && num2.Text == Mass[7])
+            {
+                Check.Content = "Bcë правильно";
+                Check.Background = Brushes.Green;
+            }
+            else if (num1.Text == Mass[5] || num2.Text == Mass[7])
+            {
+                Check.Content = "Где-то ошибка";
+                Check.Background = Brushes.Yellow;
+            }
+            else
+            {
+                Check.Content = "Неправильно";
+                Check.Background = Brushes.Red;
+            }
+        }
+
+        private void Button_Next(object sender, RoutedEventArgs e)
+        {
+            Window2 Window2 = new Window2();
+            this.Close();
+            Window2.ShowDialog();
+        }
+    }
+}
